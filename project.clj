4cca01104a1192cb3409fb2687c7656892a6d0c3
Nelson Morris <nmorris@nelsonmorris.net>
2012-03-02 23:26:31 -0600

    Bump to 0.0.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject peridot "0.0.1"
+(defproject peridot "0.0.2-SNAPSHOT"
   :description "a basic api for testing ring apps"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [ring-mock "0.1.1"]
