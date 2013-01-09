93ce76e0762ac6ce6fbb0bf65768d0303aedadee
Nelson Morris <nmorris@nelsonmorris.net>
2013-01-09 00:54:46 -0600

    Add license, bump clojure 1.5 dep

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject peridot "0.0.7-SNAPSHOT"
-  :description "a basic api for testing ring apps"
+  :description "Interact with ring apps"
   :url "https://github.com/xeqi/peridot"
   :min-lein-version "2.0.0"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [ring-mock "0.1.3"]
                  [org.clojure/data.codec "0.1.0"]
@@ -14,5 +16,5 @@
                                    [ring/ring-core "1.0.2"]]
                     :resource-paths ["test-resources"]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-alpha3"]]}}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-RC1"]]}}
   :aliases {"all" ["with-profile" "test:test,1.4:test,1.5"]})
