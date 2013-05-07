de91620000f02b93a6a2246916222563de880539
Glen Mailer <glenjamin@gmail.com>
2013-05-07 20:42:58 +0100

    Support lists in param maps

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [ring-mock "0.1.3"]
+                 [ring-mock "0.1.4"]
                  [org.clojure/data.codec "0.1.0"]
                  [org.apache.httpcomponents/httpmime "4.1.3"
                   :exclusions [commons-logging]]]
