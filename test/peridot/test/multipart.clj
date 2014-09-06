6459a0b1923d4df4beeeeb1cfbf172f98b6c73ab
Nelson Morris <nmorris@nelsonmorris.net>
2014-09-06 16:40:03 -0500

    Redo multipart tests to round trip through middleware

--- test/peridot/test/multipart.clj
+++ test/peridot/test/multipart.clj
@@ -2,6 +2,7 @@
   (:use [peridot.core]
         [clojure.test])
   (:require [peridot.multipart :as multipart]
+            [ring.middleware.multipart-params :as multiparams]
             [ring.util.response :as response]
             [clojure.java.io :as io]))
 
@@ -9,49 +10,57 @@
   (is (multipart/multipart? {"file" (io/file (io/resource "file.txt"))}))
   (is (not (multipart/multipart? {"file" "value"}))))
 
+(def ok-with-multipart-params
+  (-> (fn [req]
+        (-> (response/response "ok")
+            (assoc :multipart-params
+              (:multipart-params req))))
+      (multiparams/wrap-multipart-params)))
+
 (deftest uploading-a-file
-  (let [req (:request (-> (session (constantly (response/response "ok")))
-                          (request "/"
-                                   :request-method :post
-                                   :params {"file" (io/file (io/resource
-                                                             "file.txt"))})))]
-    (is (re-find #"multipart/form-data;"
-                 (:content-type req))
-        "files should set content-type to multipart/form-data")
-    (is (re-find #"multipart/form-data;"
-                 (get-in req [:headers "content-type"]))
-        "files should set content-type header to multipart/form-data")
-    (is (re-find #"hi from file\n" (slurp (:body req))))))
+  (let [file (io/file (io/resource "file.txt"))
+        res (-> (session ok-with-multipart-params)
+                (request "/"
+                         :request-method :post
+                         :params {"file" file})
+                :response)]
+    (let [{:keys [size filename content-type tempfile]}
+          (get-in res [:multipart-params "file"])]
+      (is (= size 13))
+      (is (= filename "file.txt"))
+      ;; TODO should this be text content type?
+      (is (= content-type "application/octet-stream"))
+      (is (= (slurp tempfile) (slurp file))))))
 
 (deftest uploading-a-file-with-keyword-keys
-  (let [req (:request (-> (session (constantly (response/response "ok")))
-                          (request "/"
-                                   :request-method :post
-                                   :params {:file (io/file (io/resource
-                                                             "file.txt"))})))]
-    (is (re-find #"multipart/form-data;"
-                 (:content-type req))
-        "files should set content-type to multipart/form-data")
-    (is (re-find #"multipart/form-data;"
-                 (get-in req [:headers "content-type"]))
-        "files should set content-type header to multipart/form-data")
-    (is (re-find #"hi from file\n" (slurp (:body req))))))
+  (let [file (io/file (io/resource "file.txt"))
+        res (-> (session ok-with-multipart-params)
+                (request "/"
+                         :request-method :post
+                         :params {:file file})
+                :response)]
+    (let [{:keys [size filename content-type tempfile]}
+          (get-in res [:multipart-params "file"])]
+      (is (= size 13))
+      (is (= filename "file.txt"))
+      ;; TODO should this be text content type?
+      (is (= content-type "application/octet-stream"))
+      (is (= (slurp tempfile) (slurp file))))))
 
 (deftest uploading-a-file-with-params
-  (let [req (:request (-> (session (constantly (response/response "ok")))
-                          (request "/"
-                                   :request-method :post
-                                   :params {"file" (io/file (io/resource "file.txt"))
-                                            "something" "☃"})))]
-    (is (re-find #"multipart/form-data;" (:content-type req))
-        "files should set content-type to multipart/form-data")
-    (is (re-find #"multipart/form-data;"
-                 (get-in req [:headers "content-type"]))
-        "files should set content-type header to multipart/form-data")
-    ;;This is a simple test
-    ;;perhaps doing a roundtrip with multipart-params middleware
-    ;;and checking response would be better
-    (let [body (slurp (:body req))]
-      (is (re-find #"hi from file" body))
-      (is (re-find #"name=\"something\"" body))
-      (is (re-find #"\r\n☃\r\n--" body)))))
+  (let [file (io/file (io/resource "file.txt"))
+        res (-> (session ok-with-multipart-params)
+                (request "/"
+                         :request-method :post
+                         :params {"file" file
+                                  "something" "☃"})
+                :response)]
+    (let [{:keys [size filename content-type tempfile]}
+          (get-in res [:multipart-params "file"])]
+      (is (= size 13))
+      (is (= filename "file.txt"))
+      ;; TODO should this be text content type?
+      (is (= content-type "application/octet-stream"))
+      (is (= (slurp tempfile) (slurp file))))
+    (is (= (get-in res [:multipart-params "something"])
+           "☃"))))
