3c3a3df16c13b4991494bcc480ede9e7636a433e
Nelson Morris <nmorris@nelsonmorris.net>
2014-09-06 16:39:28 -0500

    Bump version for development, redo profiles
    Use clojure 1.3 for pom generation.
    Use clojure 1.6 for development, due to requirements on tooling.  In particular cider.
    
    Use 1.3,1.4,1.5,1.6 based on the profile for checking against multiple clojure versions in `lein all test`.  Use + in with-profiles to keep dev profile in for testing setup.

--- project.clj
+++ project.clj
@@ -1,22 +1,28 @@
-(defproject peridot "0.3.0"
+(defproject peridot "0.3.1-SNAPSHOT"
   :description "Interact with ring apps"
   :url "https://github.com/xeqi/peridot"
   :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [;;Use clojure 1.3 for pom generation
+                 [org.clojure/clojure "1.3.0"]
                  [ring-mock "0.1.5"]
                  [org.clojure/data.codec "0.1.0"]
                  [org.apache.httpcomponents/httpmime "4.1.3"
                   :exclusions [commons-logging]]
                  [clj-time "0.5.1"]]
-  :profiles {:test {:dependencies [[net.cgrand/moustache "1.1.0"
+  :profiles {:dev {:dependencies [[net.cgrand/moustache "1.1.0"
                                     :exclusions
                                     [[org.clojure/clojure]
                                      [ring/ring-core]]]
-                                   [ring/ring-core "1.1.8"]]
+                                   [ring/ring-core "1.1.8"]
+                                   ;; use 1.6 for development
+                                   ;; cider requires it
+                                   ^:replace [org.clojure/clojure "1.6.0"]]
                     :resource-paths ["test-resources"]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
-  :aliases {"all" ["with-profile" "test:test,1.4:test,1.5:test,1.6"]})
+             ;; use the relevant clojure version for testing
+             :1.3 {:dependencies [^:replace [org.clojure/clojure "1.3.0"]]}
+             :1.4 {:dependencies [^:replace [org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [^:replace [org.clojure/clojure "1.5.1"]]}
+             :1.6 {:dependencies [^:replace [org.clojure/clojure "1.6.0"]]}}
+  :aliases {"all" ["with-profile" "+1.3:+1.4:+1.5:+1.6"]})
