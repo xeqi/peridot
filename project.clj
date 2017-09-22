0d6e76d03e34a174fb1d647422cbf1825959df09
Daniel Compton <desk@danielcompton.net>
2017-09-23 02:05:35 +1200

    Add release-tasks and bump to 0.5.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject peridot "0.4.5-SNAPSHOT"
+(defproject peridot "0.5.0-SNAPSHOT"
   :description "Interact with ring apps"
   :url "https://github.com/xeqi/peridot"
   :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [;;Use clojure 1.3 for pom generation
                  [org.clojure/clojure "1.5.1"]
                  [ring-mock "0.1.5"]
@@ -13,15 +13,24 @@
                  [org.apache.httpcomponents/httpcore "4.4.5"]
                  [clj-time "0.12.0"]]
 
-  :profiles {:dev {:dependencies [[net.cgrand/moustache "1.1.0"
-                                    :exclusions
-                                    [[org.clojure/clojure]
-                                     [ring/ring-core]]]
-                                   [ring/ring-core "1.5.0"]
-                                   [javax.servlet/servlet-api "2.5"]
-                                   ;; use 1.8 for development
-                                   ^:replace [org.clojure/clojure "1.8.0"]]
-                    :resource-paths ["test-resources"]}
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "v"]
+                  ["deploy"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
+
+  :profiles {:dev {:dependencies   [[net.cgrand/moustache "1.1.0"
+                                     :exclusions
+                                     [[org.clojure/clojure]
+                                      [ring/ring-core]]]
+                                    [ring/ring-core "1.5.0"]
+                                    [javax.servlet/servlet-api "2.5"]
+                                    ;; use 1.8 for development
+                                    ^:replace [org.clojure/clojure "1.8.0"]]
+                   :resource-paths ["test-resources"]}
              ;; use the relevant clojure version for testing
              :1.5 {:dependencies [^:replace [org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [^:replace [org.clojure/clojure "1.6.0"]]}
